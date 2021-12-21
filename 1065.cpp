3b218160175a84484643c60ca5afaa1ce348aa26
eojinsarang joo <wndjwlstkfkd@naver.com>
2021-12-22 01:41:22 +0900

    1065

---
+++ 1065.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int n, sum = 0;
+    int hund, ten, one;
+    cin>>n;
+    for(int i = 1; i <= n; i++){
+        if(i<100){
+            sum++;
+        }
+        else{
+            hund = i/100;
+            ten = i%100/10;
+            one = i%10;
+            if(hund-ten==ten-one){
+                sum++;
+            } 
+        }
+    }
+    cout<<sum;
+}
---
+++ 1065.py
@@ -0,0 +1,12 @@
+n=int(input())
+sum=0 
+
+for i in range (1, n + 1):
+    if i<100 :
+        sum+=1
+    else :
+        d = list(map(int, str(i)))
+        if d[0] - d[1] == d[1] - d[2] :
+            sum+=1
+
+print(sum)
