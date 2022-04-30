68e2c26b9f99056c2fd03bdc58dea115f4ae5965
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-04-30 11:38:30 +0900

    20220430

--- Graph Traversal/17836_공주님을 구해라!.cpp
+++ Graph Traversal/17836_공주님을 구해라!.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 int dx[4]={0,1,0,-1};
 int dy[4]={1,0,-1,0};
+char map[105][105];
+bool visited[105][105][2];
+int n, m, t;
+
+struct block{
+    int x;
+    int y;
+    int k;
+    int time;
+};
+queue<block> q;
 
 int main(){
     ios::sync_with_stdio(false);
-	cin.tie(0); cout.tie(0);
-    char map[101][101];
-    bool visited[101][101][2];
-    int n, m, t;
+	cin.tie(NULL); cout.tie(NULL);
 
     cin >> n >> m >> t;
     for(int i=0;i<n;i++){
@@ -18,39 +26,42 @@ int main(){
             cin >> map[i][j];
         }
     }
- 
-    queue<pair<pair<int,int>,pair<int,int>>> q;
     
     visited[0][0][0]=true;
-    q.push({{0,0}, {0, 0}});
+    q.push({0, 0, 0, 0});
 
     while(!q.empty()){
-        int x=q.front().first.first;
-        int y=q.front().first.second;
-        int k=q.front().second.first;
-        int time=q.front().second.second;
+        int x=q.front().x;
+        int y=q.front().y;
+        int k=q.front().k;
+        int time=q.front().time;
         q.pop();
+
+        if(map[x][y]=='2') k=1;
         if(x==n-1 && y==m-1){
-            cout << time;
+            if(time<=t) cout << time;
+            else cout << "Fail";
             return 0;
         }
-        if(time>t) continue;
         
         for(int i=0;i<4;i++){
             int nx=x+dx[i];
             int ny=y+dy[i];
             if(nx<0||nx>=n||ny<0||ny>=m) continue;
-            if(!visited[nx][ny][1] && (map[nx][ny]=='2'||k==true)){
-                visited[x][y][1]=true;
-                q.push({{nx, ny}, {1, time+1}});
+            if(k==1){
+                if(!visited[nx][ny][1]){
+                    visited[nx][ny][1]=true;
+                    q.push({nx, ny, 1, time+1});
+                }
             }
             else{
-                if (!visited[nx][ny][0] && map[nx][ny]=='0') {
-                    visited[x][y][0]=true;
-                    q.push({{nx, ny}, {0, time+1}});
+                if (!visited[nx][ny][0] && map[nx][ny] !='1') {
+                    visited[nx][ny][0]=true;
+                    q.push({nx, ny, 0, time+1});
                 }
             }
         }
     }
     cout << "Fail";
+    return 0;
 }
