acfc0b565eec5b3e478e31a7c4138be016397b76
eojinsarang joo <wndjwlstkfkd@naver.com>
2022-09-11 02:22:43 +0900

    20220911

--- BOJ 길라잡이 베타 (1)/10845_큐.cpp
+++ BOJ 길라잡이 베타 (1)/10845_큐.cpp
@@ -17,38 +17,38 @@ int main(){
         }
         if(str.compare("pop")==0){
             if(!qu.empty()){
-                cout<< qu.front()<<endl;
+                cout<< qu.front()<<"\n";
                 qu.pop();
             }
             else{
-                cout<<"-1"<<endl;
+                cout<<"-1"<<"\n";
             }
         }
         if(str.compare("size")==0){
-            cout <<qu.size() << endl;
+            cout <<qu.size() << "\n";
         }
         if(str.compare("empty")==0){
             if(!qu.empty()){
-                cout<< "0" <<endl;
+                cout<< "0" <<"\n";
             }
             else{
-                cout<<"1"<<endl;
+                cout<<"1"<<"\n";
             }
         }
         if(str.compare("front")==0){
             if(!qu.empty()){
-                cout<< qu.front() <<endl;
+                cout<< qu.front() <<"\n";
             }
             else{
-                cout<<"-1"<<endl;
+                cout<<"-1"<<"\n";
             }
         }
         if(str.compare("back")==0){
             if(!qu.empty()){
-                cout<< qu.back() <<endl;
+                cout<< qu.back() <<"\n";
             }
             else{
-                cout<<"-1"<<endl;
+                cout<<"-1"<<"\n";
             }
         }
     }
